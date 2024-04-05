166369f8285913c2aa805bc1f308026271afafa5
felix-rojas <a01201946@tec.mx>
2024-04-05 15:21:33 -0600

    check and sort text files

--- a01201946_main/main.cpp
+++ a01201946_main/main.cpp
@@ -1,11 +1,18 @@
 #include <filesystem>
 #include <iostream>
+#include <regex>
 #include <string>
+#include <vector>
 
 namespace fs = std::filesystem;
 
 int main() {
-  const std::string path = "../pruebas";
-  for (const auto &entry : fs::directory_iterator(path))
-    std::cout << entry.path() << std::endl;
+  const std::string test_paths = "../pruebas";
+  std::vector<std::filesystem::path> files;
+  for (const auto &entry : fs::directory_iterator(test_paths))
+    files.push_back(entry.path());
+
+  std::sort(files.begin(), files.end());
+  for (auto &file : files)
+    std::cout << file << std::endl;
 }
