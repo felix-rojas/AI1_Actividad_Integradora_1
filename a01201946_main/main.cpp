cd118216d1f49a398a44b1a0653455e2a22fc9b7
felix-rojas <a01201946@tec.mx>
2024-04-05 15:33:53 -0600

    regex parsing for files, save to vectors

--- a01201946_main/main.cpp
+++ a01201946_main/main.cpp
@@ -6,13 +6,26 @@
 
 namespace fs = std::filesystem;
 
+const std::string test_paths = "../pruebas";
+const std::regex transmission_pattern(".*transmission\\d*\\.txt");
+const std::regex mc_pattern(".*mcode\\d*\\.txt");
+
 int main() {
-  const std::string test_paths = "../pruebas";
-  std::vector<std::filesystem::path> files;
+  std::vector<fs::path> files;
+  std::vector<std::string> mcodes;
+  std::vector<std::string> transmissions;
   for (const auto &entry : fs::directory_iterator(test_paths))
     files.push_back(entry.path());
 
   std::sort(files.begin(), files.end());
-  for (auto &file : files)
-    std::cout << file << std::endl;
+
+  for (auto &file : files) {
+    if (std::regex_match(file.generic_string(), transmission_pattern)) {
+      transmissions.push_back(file.generic_string());
+    }
+    if (std::regex_match(file.generic_string(), mc_pattern)) {
+      mcodes.push_back(file.generic_string());
+    }
+  }
+  
 }
